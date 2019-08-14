7410e700a51b1e265c148320def6609a1768dfc1
iaroslav0 <iaroslav.vonts@gmail.com>
2019-08-14 17:15:02 +0700

    add code for section1

---
+++ 1_understand_data.py
@@ -0,0 +1,153 @@
+# import libraries
+from __future__ import division
+
+from datetime import datetime, timedelta
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import seaborn as sns
+
+import chart_studio.plotly as py
+import plotly.graph_objs as go
+import plotly.io as pio
+
+tx_data = pd.read_csv('data.csv', encoding = "ISO-8859-1")
+
+print(tx_data.head(10).to_string())
+input('Enter to continue...')
+
+######################################################
+
+#converting the type of Invoice Date Field from string to datetime.
+tx_data['InvoiceDate'] = pd.to_datetime(tx_data['InvoiceDate'])
+
+#creating YearMonth field for the ease of reporting and visualization
+tx_data['InvoiceYearMonth'] = tx_data['InvoiceDate'].map(lambda date: 100*date.year + date.month)
+
+#calculate Revenue for each row and create a new dataframe with YearMonth - Revenue columns
+tx_data['Revenue'] = tx_data['UnitPrice'] * tx_data['Quantity']
+tx_revenue = tx_data.groupby(['InvoiceYearMonth'])['Revenue'].sum().reset_index()
+print(tx_revenue.head(10).to_string())
+input('Enter to continue...')
+
+
+######################################################
+
+#X and Y axis inputs for Plotly graph. We use Scatter for line graphs
+plot_data = [
+    go.Scatter(
+        x=tx_revenue['InvoiceYearMonth'],
+        y=tx_revenue['Revenue'],
+    )
+]
+
+plot_layout = go.Layout(
+        xaxis={"type": "category"},
+        title='Montly Revenue'
+    )
+fig = go.Figure(data=plot_data, layout=plot_layout)
+pio.show(fig)
+input('Enter to continue...')
+
+
+######################################################
+
+#using pct_change() function to see monthly percentage change
+tx_revenue['MonthlyGrowth'] = tx_revenue['Revenue'].pct_change()
+
+#visualization - line graph
+plot_data = [
+    go.Scatter(
+        x=tx_revenue.query("InvoiceYearMonth < 201112")['InvoiceYearMonth'],
+        y=tx_revenue.query("InvoiceYearMonth < 201112")['MonthlyGrowth'],
+    )
+]
+
+plot_layout = go.Layout(
+        xaxis={"type": "category"},
+        title='Montly Growth Rate'
+    )
+
+fig = go.Figure(data=plot_data, layout=plot_layout)
+pio.show(fig)
+input('Enter to continue...')
+
+
+######################################################
+
+#creating a new dataframe with UK customers only
+tx_uk = tx_data.query("Country=='United Kingdom'").reset_index(drop=True)
+
+#creating monthly active customers dataframe by counting unique Customer IDs
+tx_monthly_active = tx_uk.groupby('InvoiceYearMonth')['CustomerID'].nunique().reset_index()
+
+#print the dataframe
+print(tx_monthly_active.head(10).to_string())
+
+#plotting the output
+plot_data = [
+    go.Bar(
+        x=tx_monthly_active['InvoiceYearMonth'],
+        y=tx_monthly_active['CustomerID'],
+    )
+]
+
+plot_layout = go.Layout(
+        xaxis={"type": "category"},
+        title='Monthly Active Customers'
+    )
+
+fig = go.Figure(data=plot_data, layout=plot_layout)
+pio.show(fig)
+input('Enter to continue....')
+######################################################
+
+#create a new dataframe for no. of order by using quantity field
+tx_monthly_sales = tx_uk.groupby('InvoiceYearMonth')['Quantity'].sum().reset_index()
+
+#print the dataframe
+print(tx_monthly_sales.head(10).to_string())
+
+#plot
+plot_data = [
+    go.Bar(
+        x=tx_monthly_sales['InvoiceYearMonth'],
+        y=tx_monthly_sales['Quantity'],
+    )
+]
+
+plot_layout = go.Layout(
+        xaxis={"type": "category"},
+        title='Monthly Total # of Order'
+    )
+
+fig = go.Figure(data=plot_data, layout=plot_layout)
+pio.show(fig)
+input('Enter to continue....')
+######################################################
+
+# create a new dataframe for average revenue by taking the mean of it
+tx_monthly_order_avg = tx_uk.groupby('InvoiceYearMonth')['Revenue'].mean().reset_index()
+
+#print the dataframe
+tx_monthly_order_avg
+
+#plot the bar chart
+plot_data = [
+    go.Bar(
+        x=tx_monthly_order_avg['InvoiceYearMonth'],
+        y=tx_monthly_order_avg['Revenue'],
+    )
+]
+
+plot_layout = go.Layout(
+        xaxis={"type": "category"},
+        title='Monthly Order Average'
+    )
+fig = go.Figure(data=plot_data, layout=plot_layout)
+pio.show(fig)
+
