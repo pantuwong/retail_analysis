2427be22cbbcf751bd67280d5fbcf32c7e49c95a
Natapon Pantuwong <thai.annotation@gmail.com>
2019-08-15 12:58:44 +0700

    add 4 files

---
+++ 4_next_purchase_date.py
@@ -0,0 +1,198 @@
+#import libraries
+from __future__ import division
+
+from datetime import datetime, timedelta,date
+import pandas as pd
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+
+import chart_studio.plotly as py
+import plotly.graph_objs as go
+import plotly.io as pio
+
+import xgboost as xgb
+
+#import the csv
+tx_data = pd.read_csv('data.csv', encoding = "ISO-8859-1")
+
+#convert date field from string to datetime
+tx_data['InvoiceDate'] = pd.to_datetime(tx_data['InvoiceDate'])
+
+#create dataframe with uk data only
+tx_uk = tx_data.query("Country=='United Kingdom'").reset_index(drop=True)
+
+#create dataframe for first 6 month and next 3 month
+tx_6m = tx_uk[(tx_uk.InvoiceDate < date(2011,9,1)) & (tx_uk.InvoiceDate >= date(2011,3,1))].reset_index(drop=True)
+tx_next = tx_uk[(tx_uk.InvoiceDate >= date(2011,9,1)) & (tx_uk.InvoiceDate < date(2011,12,1))].reset_index(drop=True)
+
+#create dataframe for user
+tx_user = pd.DataFrame(tx_6m['CustomerID'].unique())
+tx_user.columns = ['CustomerID']
+
+#create a dataframe with customer id and first purchase date in tx_next
+tx_next_first_purchase = tx_next.groupby('CustomerID').InvoiceDate.min().reset_index()
+tx_next_first_purchase.columns = ['CustomerID','MinPurchaseDate']
+
+#create a dataframe with customer id and last purchase date in tx_6m
+tx_last_purchase = tx_6m.groupby('CustomerID').InvoiceDate.max().reset_index()
+tx_last_purchase.columns = ['CustomerID','MaxPurchaseDate']
+
+#merge two dataframes
+tx_purchase_dates = pd.merge(tx_last_purchase,tx_next_first_purchase,on='CustomerID',how='left')
+
+#calculate the time difference in days:
+tx_purchase_dates['NextPurchaseDay'] = (tx_purchase_dates['MinPurchaseDate'] - tx_purchase_dates['MaxPurchaseDate']).dt.days
+
+#merge with tx_user 
+tx_user = pd.merge(tx_user, tx_purchase_dates[['CustomerID','NextPurchaseDay']],on='CustomerID',how='left')
+
+#print tx_user
+print('Before fillna')
+print(tx_user.head(10).to_string())
+
+#fill NA values with 999
+tx_user = tx_user.fillna(999)
+
+#print tx_user
+print('After fillna')
+print(tx_user.head(10).to_string())
+input('Enter to continue....')
+###########################################################################################
+
+#get max purchase date for Recency and create a dataframe
+tx_max_purchase = tx_6m.groupby('CustomerID').InvoiceDate.max().reset_index()
+tx_max_purchase.columns = ['CustomerID','MaxPurchaseDate']
+
+#find the recency in days and add it to tx_user
+tx_max_purchase['Recency'] = (tx_max_purchase['MaxPurchaseDate'].max() - tx_max_purchase['MaxPurchaseDate']).dt.days
+tx_user = pd.merge(tx_user, tx_max_purchase[['CustomerID','Recency']], on='CustomerID')
+
+#clustering for Recency
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(tx_user[['Recency']])
+tx_user['RecencyCluster'] = kmeans.predict(tx_user[['Recency']])
+
+#order cluster method
+def order_cluster(cluster_field_name, target_field_name,df,ascending):
+    new_cluster_field_name = 'new_' + cluster_field_name
+    df_new = df.groupby(cluster_field_name)[target_field_name].mean().reset_index()
+    df_new = df_new.sort_values(by=target_field_name,ascending=ascending).reset_index(drop=True)
+    df_new['index'] = df_new.index
+    df_final = pd.merge(df,df_new[[cluster_field_name,'index']], on=cluster_field_name)
+    df_final = df_final.drop([cluster_field_name],axis=1)
+    df_final = df_final.rename(columns={"index":cluster_field_name})
+    return df_final
+
+
+#order recency clusters
+tx_user = order_cluster('RecencyCluster', 'Recency',tx_user,False)
+
+#print cluster characteristics
+tx_user.groupby('RecencyCluster')['Recency'].describe()
+
+
+#get total purchases for frequency scores
+tx_frequency = tx_6m.groupby('CustomerID').InvoiceDate.count().reset_index()
+tx_frequency.columns = ['CustomerID','Frequency']
+
+#add frequency column to tx_user
+tx_user = pd.merge(tx_user, tx_frequency, on='CustomerID')
+
+#clustering for frequency
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(tx_user[['Frequency']])
+tx_user['FrequencyCluster'] = kmeans.predict(tx_user[['Frequency']])
+
+#order frequency clusters and show the characteristics
+tx_user = order_cluster('FrequencyCluster', 'Frequency',tx_user,True)
+tx_user.groupby('FrequencyCluster')['Frequency'].describe()
+
+
+#calculate monetary value, create a dataframe with it
+tx_6m['Revenue'] = tx_6m['UnitPrice'] * tx_6m['Quantity']
+tx_revenue = tx_6m.groupby('CustomerID').Revenue.sum().reset_index()
+
+#add Revenue column to tx_user
+tx_user = pd.merge(tx_user, tx_revenue, on='CustomerID')
+
+#Revenue clusters 
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(tx_user[['Revenue']])
+tx_user['RevenueCluster'] = kmeans.predict(tx_user[['Revenue']])
+
+#ordering clusters and who the characteristics
+tx_user = order_cluster('RevenueCluster', 'Revenue',tx_user,True)
+tx_user.groupby('RevenueCluster')['Revenue'].describe()
+
+
+#building overall segmentation
+tx_user['OverallScore'] = tx_user['RecencyCluster'] + tx_user['FrequencyCluster'] + tx_user['RevenueCluster']
+
+#assign segment names
+tx_user['Segment'] = 'Low-Value'
+tx_user.loc[tx_user['OverallScore']>2,'Segment'] = 'Mid-Value' 
+tx_user.loc[tx_user['OverallScore']>4,'Segment'] = 'High-Value' 
+
+print(tx_user.head(10).to_string())
+input('Enter to continue....')
+###########################################################################################
+
+#create a dataframe with CustomerID and Invoice Date
+tx_day_order = tx_6m[['CustomerID','InvoiceDate']]
+
+#convert Invoice Datetime to day
+tx_day_order['InvoiceDay'] = tx_6m['InvoiceDate'].dt.date
+tx_day_order = tx_day_order.sort_values(['CustomerID','InvoiceDate'])
+
+#drop duplicates
+tx_day_order = tx_day_order.drop_duplicates(subset=['CustomerID','InvoiceDay'],keep='first')
+
+#shifting last 3 purchase dates
+tx_day_order['PrevInvoiceDate'] = tx_day_order.groupby('CustomerID')['InvoiceDay'].shift(1)
+tx_day_order['T2InvoiceDate'] = tx_day_order.groupby('CustomerID')['InvoiceDay'].shift(2)
+tx_day_order['T3InvoiceDate'] = tx_day_order.groupby('CustomerID')['InvoiceDay'].shift(3)
+
+tx_day_order['DayDiff'] = (tx_day_order['InvoiceDay'] - tx_day_order['PrevInvoiceDate']).dt.days
+tx_day_order['DayDiff2'] = (tx_day_order['InvoiceDay'] - tx_day_order['T2InvoiceDate']).dt.days
+tx_day_order['DayDiff3'] = (tx_day_order['InvoiceDay'] - tx_day_order['T3InvoiceDate']).dt.days
+print('... tx_day_order ...')
+print(tx_day_order.head(5).to_string())
+
+tx_day_diff = tx_day_order.groupby('CustomerID').agg({'DayDiff': ['mean','std']}).reset_index()
+tx_day_diff.columns = ['CustomerID', 'DayDiffMean','DayDiffStd']
+print('... tx_day_diff ...')
+print(tx_day_diff.head(5).to_string())
+
+tx_day_order_last = tx_day_order.drop_duplicates(subset=['CustomerID'],keep='last')
+tx_day_order_last = tx_day_order_last.dropna()
+tx_day_order_last = pd.merge(tx_day_order_last, tx_day_diff, on='CustomerID')
+tx_user = pd.merge(tx_user, tx_day_order_last[['CustomerID','DayDiff','DayDiff2','DayDiff3','DayDiffMean','DayDiffStd']], on='CustomerID')
+#create tx_class as a copy of tx_user before applying get_dummies
+tx_class = tx_user.copy()
+tx_class = pd.get_dummies(tx_class)
+
+tx_class['NextPurchaseDayRange'] = 2
+tx_class.loc[tx_class.NextPurchaseDay>20,'NextPurchaseDayRange'] = 1
+tx_class.loc[tx_class.NextPurchaseDay>50,'NextPurchaseDayRange'] = 0
+
+print('... tx_class ...')
+print(tx_class.head(5).to_string())
+input('Enter to continue....')
+
+###########################################################################################
+
+tx_class = tx_class.drop('NextPurchaseDay',axis=1)
+X, y = tx_class.drop('NextPurchaseDayRange',axis=1), tx_class.NextPurchaseDayRange
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=44)
+xgb_model = xgb.XGBClassifier().fit(X_train, y_train)
+print('Accuracy of XGB classifier on training set: {:.2f}'
+       .format(xgb_model.score(X_train, y_train)))
+print('Accuracy of XGB classifier on test set: {:.2f}'
+       .format(xgb_model.score(X_test[X_train.columns], y_test)))
